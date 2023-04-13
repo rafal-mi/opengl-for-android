f4068937efb23d25c778a4b33f3b28dce01982d3
Rafal Mi <rafal.m4@gmail.com>
2023-04-13 23:23:45 +0200

    End of Chapter 3 Compiling Shaders and Drawing to the Screen

--- ch02/AirHockey1/app/src/main/java/com/airhockey/android/AirHockeyRenderer.kt
+++ ch02/AirHockey1/app/src/main/java/com/airhockey/android/AirHockeyRenderer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLES20.*
 import android.opengl.GLSurfaceView.Renderer
+import com.airhockey.android.util.LoggerConfig
 import com.airhockey.android.util.ShaderHelper
 import com.airhockey.android.util.TextResourceReader
 import java.nio.ByteBuffer
@@ -14,36 +15,41 @@ import javax.microedition.khronos.opengles.GL10
 
 class AirHockeyRenderer(private val context: Context) : Renderer {
     var vertexData: FloatBuffer? = null
+    private var program = -1
+    private var uColorLocation = -1
+    private var aPositionLocation = -1
 
     init {
-        val tableVerticesWithTriangles = arrayOf(
+        val tableVerticesWithTriangles = floatArrayOf(
             // Triangle 1
-            0f, 0f,
-            9f, 14f,
-            0f, 14f,
+            -0.5f, -0.5f,
+            0.5f, 0.5f,
+            -0.5f, 0.5f,
 
             // Triangle 2
-            0f, 0f,
-            9f, 0f,
-            9f, 14f,
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.5f, 0.5f,
 
             // Line 1
-            0f, 7f,
-            9f, 7f,
+            -0.5f, 0f,
+            0.5f, 0f,
 
             // Mallets
-            4.5f, 2f,
-            4.5f, 12f
+            0f, -0.25f,
+            0f, 0.25f
         )
 
         vertexData = ByteBuffer
             .allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT)
             .order(ByteOrder.nativeOrder())
             .asFloatBuffer()
+
+        (vertexData as FloatBuffer).put(tableVerticesWithTriangles)
     }
 
     override fun onSurfaceCreated(glUnused: GL10?, config: EGLConfig?) {
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
 
         val vertexShaderSource = TextResourceReader
             .readTextFileFromResource(context, R.raw.simple_vertex_shader)
@@ -52,6 +58,23 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
 
         val vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource)
         val fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource)
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader)
+
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program)
+        }
+
+        glUseProgram(program)
+
+        uColorLocation = glGetUniformLocation(program, U_COLOR)
+        aPositionLocation = glGetAttribLocation(program, A_POSITION)
+
+        vertexData!!.position(0)
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+            false, 0, vertexData)
+
+        glEnableVertexAttribArray(aPositionLocation)
     }
 
     override fun onSurfaceChanged(glUnused: GL10?, width: Int, height: Int) {
@@ -59,12 +82,30 @@ class AirHockeyRenderer(private val context: Context) : Renderer {
     }
 
     override fun onDrawFrame(glUnused: GL10?) {
-        glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f)
+        glDrawArrays(GL_TRIANGLES, 0, 6)
+
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
+        glDrawArrays(GL_LINES, 6, 2)
+
+        // Draw the first mallet blue.
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f)
+        glDrawArrays(GL_POINTS, 8, 1)
+
+        // Draw the second mallet red.
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
+        glDrawArrays(GL_POINTS, 9, 1)
     }
 
     companion object {
-        val POSITION_COMPONENT_COUNT = 2
+        const val POSITION_COMPONENT_COUNT = 2
 
-        val BYTES_PER_FLOAT = 4
+        const val BYTES_PER_FLOAT = 4
+
+        const val U_COLOR = "u_Color"
+
+        const val A_POSITION = "a_Position"
     }
 }
--- ch02/AirHockey1/app/src/main/java/com/airhockey/android/util/ShaderHelper.kt
+++ ch02/AirHockey1/app/src/main/java/com/airhockey/android/util/ShaderHelper.kt
@@ -17,7 +17,7 @@ class ShaderHelper {
 
             if (shaderObjectId == 0) {
                 if (LoggerConfig.ON) {
-                    Log.w(TAG, "Could not create new shader.");
+                    Log.w(TAG, "Could not create new shader.")
                 }
                 return 0
             }
@@ -31,10 +31,68 @@ class ShaderHelper {
             if (LoggerConfig.ON) {
                 // Print the shader info log to the Android log output.
                 Log.v(TAG, "Results of compiling source:" + "\n" + shaderCode + "\n:"
-                        + glGetShaderInfoLog(shaderObjectId));
+                        + glGetShaderInfoLog(shaderObjectId))
+            }
+
+            if (compileStatus[0] == 0) {
+                // If it failed, delete the shader object.
+                glDeleteShader(shaderObjectId);
+
+                if (LoggerConfig.ON) {
+                    Log.w(TAG, "Compilation of shader failed.");
+                }
+
+                return 0;
             }
 
             return shaderObjectId
+        }
+
+        fun linkProgram(vertexShaderId: Int, fragmentShaderId: Int): Int {
+            val programObjectId = glCreateProgram()
+
+            if (programObjectId == 0) {
+                if (LoggerConfig.ON) {
+                    Log.w(TAG, "Could not create new program")
+                }
+                return 0
+            }
+
+            glAttachShader(programObjectId, vertexShaderId)
+            glAttachShader(programObjectId, fragmentShaderId)
+
+            glLinkProgram(programObjectId)
+
+            val linkStatus = IntArray(1)
+            glGetProgramiv(programObjectId, GL_LINK_STATUS, linkStatus, 0)
+
+            if (LoggerConfig.ON) {
+                // Print the program info log to the Android log output.
+                Log.v(TAG, "Results of linking program:\n"
+                        + glGetProgramInfoLog(programObjectId));
+            }
+
+            if (linkStatus[0] == 0) {
+                // If it failed, delete the program object.
+                glDeleteProgram(programObjectId);
+                if (LoggerConfig.ON) {
+                    Log.w(TAG, "Linking of program failed.")
+                }
+                return 0
+            }
+
+            return programObjectId
+        }
+
+        fun validateProgram(programObjectId: Int): Boolean {
+            glValidateProgram(programObjectId)
+            val validateStatus = IntArray(1)
+            glGetProgramiv(programObjectId, GL_VALIDATE_STATUS, validateStatus, 0)
+
+            Log.v(TAG, "Results of validating program: " + validateStatus[0]
+                    + "\nLog:" + glGetProgramInfoLog(programObjectId))
+
+            return validateStatus[0] != 0
         }
 
     }
