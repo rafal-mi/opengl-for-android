8a894084610c46f9a02262cbbc37238bc7e401d7
Rafal Mi <rafal.m4@gmail.com>
2023-04-15 16:51:17 +0200

    Created project AirHockeyOrtho

---
+++ сh04/AirHockeyOrtho/app/src/main/java/com/airhockey/android/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.airhockey.android
+
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.airhockey.android.ui.theme.AirHockeyOrthoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AirHockeyOrthoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .weight(1f)
+                .padding(8.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column() {
+                Text(text = stringResource(id = R.string.app_name), fontSize = 32.sp)
+            }
+        }
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(3f),
+            factory = {
+                GLSurfaceView(it).apply {
+                    setEGLContextClientVersion(2)
+                    setRenderer(AirHockeyRenderer(context))
+                }
+            }
+        )
+    }
+}
