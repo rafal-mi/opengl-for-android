12a84da2a7853d1d2841f6844ef43c90ec1064f8
Rafal Mi <rafal.m4@gmail.com>
2023-04-17 13:28:30 +0200

    End of Chapter 6 Entering the Third Dimension

---
+++ ch05/AirHockey3D/app/src/main/java/com/airhockey/android/AirHockeyRenderer.kt
@@ -0,0 +1,129 @@
+package com.airhockey.android
+
+import android.content.Context
+import android.opengl.GLES20
+import android.opengl.GLES20.*
+import android.opengl.GLSurfaceView.Renderer
+import android.opengl.Matrix.*
+import com.airhockey.android.util.LoggerConfig
+import com.airhockey.android.util.MatrixHelper
+import com.airhockey.android.util.ShaderHelper
+import com.airhockey.android.util.TextResourceReader
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class AirHockeyRenderer(private val context: Context) : Renderer {
+    var vertexData: FloatBuffer? = null
+    private var projectionMatrix = FloatArray(16)
+    private var modelMatrix = FloatArray(16)
+
+    private var program = -1
+    // private var uColorLocation = -1
+    private var aPositionLocation = -1
+    private var aColorLocation = -1
+    private var uMatrixLocation = -1
+
+    init {
+        val tableVerticesWithTriangles = floatArrayOf(
+            // Order of coordinates: X, Y, R, G, B
+
+            // Triangle Fan
+            0f,    0f,   1f,   1f,   1f,
+            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+            0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+            -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+
+            // Line 1
+            -0.5f, 0f, 1f, 0f, 0f,
+            0.5f, 0f, 1f, 0f, 0f,
+
+            // Mallets
+            0f, -0.25f, 0f, 0f, 1f,
+            0f,  0.25f, 1f, 0f, 0f
+        )
+
+        vertexData = ByteBuffer
+            .allocateDirect(tableVerticesWithTriangles.size * BYTES_PER_FLOAT)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+
+        (vertexData as FloatBuffer).put(tableVerticesWithTriangles)
+    }
+
+    override fun onSurfaceCreated(glUnused: GL10?, config: EGLConfig?) {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+
+        val vertexShaderSource = TextResourceReader
+            .readTextFileFromResource(context, R.raw.simple_vertex_shader)
+        val fragmentShaderSource = TextResourceReader
+            .readTextFileFromResource(context, R.raw.simple_fragment_shader)
+
+        val vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource)
+        val fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource)
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader)
+
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program)
+        }
+
+        glUseProgram(program)
+
+        aPositionLocation = glGetAttribLocation(program, A_POSITION)
+        aColorLocation = glGetAttribLocation(program, A_COLOR)
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX)
+
+        vertexData!!.position(0)
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+            false, STRIDE, vertexData)
+
+        glEnableVertexAttribArray(aPositionLocation)
+
+        vertexData!!.position(POSITION_COMPONENT_COUNT)
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
+            false, STRIDE, vertexData)
+
+        glEnableVertexAttribArray(aColorLocation)
+    }
+
+    override fun onSurfaceChanged(glUnused: GL10?, width: Int, height: Int) {
+        glViewport(0, 0, width, height)
+        MatrixHelper.perspectiveM(projectionMatrix, 45.toFloat(), width.toFloat() / height.toFloat(), 1f, 10f)
+        setIdentityM(modelMatrix, 0)
+        translateM(modelMatrix, 0, 0f, 0f, -3.0f)
+        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f)
+        val temp = FloatArray(16)
+        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0)
+        System.arraycopy(temp, 0, projectionMatrix, 0, temp.size)
+    }
+
+    override fun onDrawFrame(glUnused: GL10?) {
+        glClear(GL_COLOR_BUFFER_BIT)
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0)
+
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6)
+        glDrawArrays(GL_LINES, 6, 2)
+
+        // Draw the first mallet blue.
+        glDrawArrays(GL_POINTS, 8, 1)
+
+        // Draw the second mallet red.
+        glDrawArrays(GL_POINTS, 9, 1)
+    }
+
+    companion object {
+        const val A_POSITION = "a_Position"
+        const val A_COLOR = "a_Color"
+        const val U_MATRIX = "u_Matrix"
+        const val POSITION_COMPONENT_COUNT = 2
+        const val COLOR_COMPONENT_COUNT = 3
+        const val BYTES_PER_FLOAT = 4
+        const val STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT
+    }
+}
