28a35b82ebc1964e9581565f77edee82f261e2cf
Rafal Mi <rafal.m4@gmail.com>
2023-04-27 00:21:19 +0200

    Some classes in Chapter 9

--- ch09/AirHockeyTouch/app/src/main/java/com/airhockey/android/MainActivity.kt
+++ ch09/AirHockeyTouch/app/src/main/java/com/airhockey/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.airhockey.android
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -15,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.airhockey.android.ui.theme.AirHockeyTouchTheme
 
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
-    val context = LocalContext.current
+    val airHockeyRenderer = AirHockeyRenderer(LocalContext.current)
 
     Column(modifier = Modifier.fillMaxSize()) {
         TopAppBar(
@@ -54,7 +54,29 @@ fun MainContent() {
             factory = {
                 GLSurfaceView(it).apply {
                     setEGLContextClientVersion(2)
-                    setRenderer(AirHockeyRenderer(context))
+                    setRenderer(airHockeyRenderer)
+                    setOnTouchListener { v, event ->
+//                        if(event.action == MotionEvent.ACTION_DOWN)
+//                            v.performClick()
+                        val normalizedX = (event.x / v.width.toFloat()) * 2 - 1
+                        val normalizedY = -(event.y / v.height.toFloat() * 2 - 1)
+                        if (event.action == MotionEvent.ACTION_DOWN) {
+                            v.performClick()
+                            this.queueEvent {
+//                                airHockeyRenderer.handleTouchPress(
+//                                    normalizedX, normalizedY
+//                                )
+                            }
+                        } else if (event.action == MotionEvent.ACTION_MOVE) {
+                            this.queueEvent {
+//                                airHockeyRenderer.handleTouchDrag(
+//                                    normalizedX, normalizedY
+//                                )
+                            }
+                        }
+                        true
+                    }
+
                 }
             }
         )
--- ch09/AirHockeyTouch/app/src/main/java/com/airhockey/android/util/Geometry.kt
+++ ch09/AirHockeyTouch/app/src/main/java/com/airhockey/android/util/Geometry.kt
@@ -1,5 +1,8 @@
 package com.airhockey.android.util
 
+import java.util.*
+import kotlin.math.sqrt
+
 sealed class Geometry {
     data class Point(
         val x: Float,
@@ -9,6 +12,28 @@ sealed class Geometry {
     ): Geometry() {
         fun translateY(distance: Float): Point =
             Point(x, y + distance, z)
+    }
+
+    data class Vector(
+        val x: Float,
+        val y: Float,
+        val z: Float
+    ) {
+        val length: Float
+            get() = sqrt(x * x  +  y * y  +  z * z)
+
+        fun crossProduct(other: Vector): Vector = Vector(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+            )
+
+        operator fun times(other: Vector): Float =
+            x * other.x + y * other.y + z * other.z
+
+        operator fun times(f: Float): Vector =
+            Vector(x * f, y * f, z * f)
+
     }
 
     data class Circle(
@@ -24,4 +49,9 @@ sealed class Geometry {
         val radius: Float,
         val height: Float
     ) : Geometry()
+
+    data class Ray(
+        val point: Point,
+        val vector: Vector
+    )
 }
