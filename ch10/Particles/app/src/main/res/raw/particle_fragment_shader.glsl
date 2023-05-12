5a6a29b7aa3aee71dcb4461261bc4d69288ba249
Rafal Mi <rafal.m4@gmail.com>
2023-05-12 16:36:14 +0200

    In Chapter 10, completed 10.5 Adding Gravity

--- ch10/Particles/app/src/main/res/raw/particle_fragment_shader.glsl
+++ ch10/Particles/app/src/main/res/raw/particle_fragment_shader.glsl
@@ -8,7 +8,8 @@ void main()
     float xDistance = 0.5 - gl_PointCoord.x;
     float yDistance = 0.5 - gl_PointCoord.y;
     float distanceFromCenter = 
-        sqrt(xDistance * xDistance + yDistance * yDistance);
+        sqrt(xDist
+ce * xDistance + yDistance * yDistance);
     gl_FragColor = vec4(v_Color / v_ElapsedTime, 1.0);
     
     if (distanceFromCenter > 0.5) {
