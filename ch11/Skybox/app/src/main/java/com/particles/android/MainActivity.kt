20a6a80752f63db860b9ddb6c1a7b75aa277183e
Rafal Mi <rafal.m4@gmail.com>
2023-05-19 17:15:46 +0200

    End of Chapter 11 Adding a Skybox

--- ch11/Skybox/app/src/main/java/com/particles/android/MainActivity.kt
+++ ch11/Skybox/app/src/main/java/com/particles/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.particles.android
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -15,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.particles.android.ui.theme.ParticlesTheme
 
@@ -55,6 +55,23 @@ fun MainContent() {
                 GLSurfaceView(it).apply {
                     setEGLContextClientVersion(2)
                     setRenderer(renderer)
+                    var previousX = 0f
+                    var previousY = 0f
+                    setOnTouchListener { v, event ->
+
+                        if (event.action == MotionEvent.ACTION_DOWN) {
+                            v.performClick()
+                            previousX = event.x
+                            previousY = event.y
+                        } else if (event.action == MotionEvent.ACTION_MOVE) {
+                            val deltaX = event.x - previousX
+                            val deltaY = event.y - previousY
+                            this.queueEvent {
+                                renderer.handleTouchDrag(deltaX, deltaY)
+                            }
+                        }
+                        true
+                    }
                 }
             }
         )
--- ch11/Skybox/app/src/main/java/com/particles/android/ParticlesRenderer.kt
+++ ch11/Skybox/app/src/main/java/com/particles/android/ParticlesRenderer.kt
@@ -2,12 +2,25 @@ package com.particles.android
 
 import android.content.Context
 import android.graphics.Color
-import android.opengl.GLES20.*
+import android.opengl.GLES20.GL_BLEND
+import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
+import android.opengl.GLES20.GL_ONE
+import android.opengl.GLES20.glBlendFunc
+import android.opengl.GLES20.glClear
+import android.opengl.GLES20.glClearColor
+import android.opengl.GLES20.glDisable
+import android.opengl.GLES20.glEnable
+import android.opengl.GLES20.glViewport
 import android.opengl.GLSurfaceView.Renderer
-import android.opengl.Matrix.*
+import android.opengl.Matrix.multiplyMM
+import android.opengl.Matrix.rotateM
+import android.opengl.Matrix.setIdentityM
+import android.opengl.Matrix.translateM
 import com.particles.android.objects.ParticleShooter
 import com.particles.android.objects.ParticleSystem
+import com.particles.android.objects.Skybox
 import com.particles.android.programs.ParticleShaderProgram
+import com.particles.android.programs.SkyboxShaderProgram
 import com.particles.android.util.Geometry
 import com.particles.android.util.Geometry.Point
 import com.particles.android.util.MatrixHelper
@@ -20,19 +33,31 @@ class ParticlesRenderer(private val context: Context) : Renderer {
     private val projectionMatrix = FloatArray(16)
     private val viewMatrix = FloatArray(16)
     private val viewProjectionMatrix = FloatArray(16)
+
+    private var skyboxProgram: SkyboxShaderProgram? = null
+    private var skybox: Skybox? = null
+
     private var particleProgram: ParticleShaderProgram? = null
     var particleSystem: ParticleSystem? = null
     var redParticleShooter: ParticleShooter? = null
     var greenParticleShooter: ParticleShooter? = null
     var blueParticleShooter: ParticleShooter? = null
+
     var globalStartTime: Long = 0
-    private var texture: Int = 0
+    private var particleTexture: Int = 0
+    private var skyboxTexture = 0
 
     val angleVarianceInDegrees = 5f
     val speedVariance = 1f
 
+    var xRotation = 0f
+    var yRotation = 0f
+
     override fun onSurfaceCreated(glUnused: GL10?, config: EGLConfig?) {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+
+        skyboxProgram = SkyboxShaderProgram(context)
+        skybox = Skybox()
 
         // Enable additive blending
         glEnable(GL_BLEND)
@@ -71,7 +96,25 @@ class ParticlesRenderer(private val context: Context) : Renderer {
             speedVariance
         )
 
-        texture = TextureHelper.loadTexture(context, R.drawable.particle_texture)
+        particleTexture = TextureHelper.loadTexture(context, R.drawable.particle_texture)
+
+        skyboxTexture = TextureHelper.loadCubeMap(
+            context, intArrayOf(
+                R.drawable.left, R.drawable.right,
+                R.drawable.bottom, R.drawable.top,
+                R.drawable.front, R.drawable.back
+            )
+        )
+    }
+
+    fun handleTouchDrag(deltaX: Float, deltaY: Float) {
+        xRotation += deltaX / 1024f
+        yRotation += deltaY / 1024f
+        if (yRotation < -90) {
+            yRotation = -90f
+        } else if (yRotation > 90) {
+            yRotation = 90f
+        }
     }
 
     override fun onSurfaceChanged(glUnused: GL10?, width: Int, height: Int) {
@@ -80,26 +123,49 @@ class ParticlesRenderer(private val context: Context) : Renderer {
             projectionMatrix, 45f, width.toFloat()
                     / height.toFloat(), 1f, 10f
         )
-        setIdentityM(viewMatrix, 0)
-        translateM(viewMatrix, 0, 0f, -1.5f, -5f)
-        multiplyMM(
-            viewProjectionMatrix, 0, projectionMatrix, 0,
-            viewMatrix, 0
-        )
     }
 
     override fun onDrawFrame(glUnused: GL10?) {
         glClear(GL_COLOR_BUFFER_BIT)
 
-        val currentTime = (System.nanoTime() - globalStartTime) / 1000000000f
-
-        redParticleShooter!!.addParticles(particleSystem!!, currentTime, 5)
-        greenParticleShooter!!.addParticles(particleSystem!!, currentTime, 5)
-        blueParticleShooter!!.addParticles(particleSystem!!, currentTime, 5)
-
-        particleProgram!!.useProgram();
-        particleProgram!!.setUniforms(viewProjectionMatrix, currentTime, texture);
-        particleSystem!!.bindData(particleProgram!!);
-        particleSystem!!.draw();
+        drawSkybox();
+        drawParticles();
     }
+
+    private fun drawSkybox() {
+        setIdentityM(viewMatrix, 0);
+        rotateM(viewMatrix, 0, -yRotation, 1f, 0f, 0f);
+        rotateM(viewMatrix, 0, -xRotation, 0f, 1f, 0f);
+        // translateM(viewMatrix, 0, 0f, -1.5f, -5f);
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        skyboxProgram!!.useProgram();
+        skyboxProgram!!.setUniforms(viewProjectionMatrix, skyboxTexture);
+        skybox!!.bindData(skyboxProgram!!)
+        skybox!!.draw();
+    }
+
+    private fun drawParticles() {
+        val currentTime = (System.nanoTime() - globalStartTime) / 1000000000f
+        redParticleShooter!!.addParticles(particleSystem!!, currentTime, 1)
+        greenParticleShooter!!.addParticles(particleSystem!!, currentTime, 1)
+        blueParticleShooter!!.addParticles(particleSystem!!, currentTime, 1)
+
+        setIdentityM(viewMatrix, 0)
+        rotateM(viewMatrix, 0, -yRotation, 1f, 0f, 0f)
+        rotateM(viewMatrix, 0, -xRotation, 0f, 1f, 0f)
+        translateM(viewMatrix, 0, 0f, -1.5f, -5f)
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_ONE, GL_ONE)
+
+        particleProgram!!.useProgram()
+        particleProgram!!.setUniforms(viewProjectionMatrix, currentTime, particleTexture)
+        particleSystem!!.bindData(particleProgram!!)
+        particleSystem!!.draw()
+
+        glDisable(GL_BLEND)
+    }
+
+
 }
